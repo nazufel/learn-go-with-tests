279e6762a300488643901510fed278f9bbec9040
nazufel <nazufel@gmail.com>
2022-06-22 22:19:41 -0400

    passing assertions

--- 07-maps/dictionary_test.go
+++ 07-maps/dictionary_test.go
@@ -3,13 +3,25 @@ package main
 import "testing"
 
 func TestSearch(t *testing.T) {
-	dictionary := map[string]string{"test": "this is just a test"}
+	dictionary := Dictionary{"test": "this is just a test"}
 
-	got := Search(dictionary, "test")
+	t.Run("known word", func(t *testing.T) {
+		got, _ := dictionary.Search("test")
+		want := "this is just a test"
 
-	want := "this is just a test"
+		assertStrings(t, got, want)
+	})
 
-	assertStrings(t, got, want)
+	t.Run("unknown word", func(t *testing.T) {
+		_, err := dictionary.Search("unknown")
+		want := "could not find the word you were looking for"
+
+		if err == nil {
+			t.Fatal("expected to get an error.")
+		}
+
+		assertStrings(t, err.Error(), want)
+	})
 }
 
 func assertStrings(t testing.TB, got, want string) {
@@ -17,5 +29,13 @@ func assertStrings(t testing.TB, got, want string) {
 
 	if got != want {
 		t.Errorf("got: %q, want: %q", got, want)
+	}
+}
+
+func assertError(t testing.TB, got, want error) {
+	t.Helper()
+
+	if got != want {
+		t.Errorf("got error: %q, want: %q", got, want)
 	}
 }
